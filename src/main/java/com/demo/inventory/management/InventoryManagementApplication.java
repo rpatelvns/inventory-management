aedcb813e180389af35720a2c0570dd397e944fb
Ravindra Patel <ravipatel.write@gmail.com>
2024-10-20 22:00:49 +0530

    initial version

--- src/main/java/com/demo/inventory/management/InventoryManagementApplication.java
+++ src/main/java/com/demo/inventory/management/InventoryManagementApplication.java
@@ -2,10 +2,21 @@ package com.demo.inventory.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventoryManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/v3/api-docs").allowedOrigins("*");
+			}
+		};
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ insert into stocks (sku_id,sku_name, qty) values ('09454912-3d01-4ab4-920b-26aa5
 insert into stocks (sku_id,sku_name, qty) values ('8fd78f09-66a5-4864-869d-ec67d0092c0e','Product 2', 0);
 insert into stocks (sku_id,sku_name, qty) values ('2c28b975-fd16-43aa-9551-4459aa19b86a','Product 3', 0);
 
-insert into orders (orderId) values ('bcb4045c-d4e8-4cc4-bdb8-646c679307c8');
-insert into orders (orderId) values ('dabfbfef-e133-42bc-bb86-08c71742c493');
+insert into orders (order_id, created) values ('bcb4045c-d4e8-4cc4-bdb8-646c679307c8', null);
+insert into orders (order_id, created) values ('dabfbfef-e133-42bc-bb86-08c71742c493', null);
